2d1d8d820e3763f20e2ca891695e8da25f9701d0
jsStar580 <jonathanman810@gmail.com>
2023-12-22 17:22:11 +0900

    add: NEAR nft-contract skeleton

---
+++ nft-contract/src/metadata.rs
@@ -0,0 +1,55 @@
+use crate::*;
+pub type TokenId = String;
+//defines the payout type we'll be returning as a part of the royalty standards.
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Payout {
+    pub payout: HashMap<AccountId, U128>,
+} 
+
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub struct NFTContractMetadata {
+    /*
+        FILL THIS IN
+    */
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct TokenMetadata {
+    /*
+        FILL THIS IN
+    */
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Token {
+    /*
+        FILL THIS IN
+    */
+}
+
+//The Json token is what will be returned from view calls. 
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct JsonToken {
+    /*
+        FILL THIS IN
+    */
+}
+
+pub trait NonFungibleTokenMetadata {
+    //view call for returning the contract metadata
+    fn nft_metadata(&self) -> NFTContractMetadata;
+}
+
+#[near_bindgen]
+impl NonFungibleTokenMetadata for Contract {
+    fn nft_metadata(&self) -> NFTContractMetadata {
+        /*
+            FILL THIS IN
+        */
+        todo!(); //remove once code is filled in.
+    }
+}
